d060aac50e131bbcda698a0e9a1f0418e033d443
Michael Prather <mprather1@gmail.com>
2016-04-03 00:43:33 -0500

    Users model and controller

---
+++ app/controllers/assignments_controller.rb
@@ -0,0 +1,55 @@
+class AssignmentsController < ApplicationController
+
+  def index
+    @assignments = Assignment.all
+  end
+
+  def show
+    @assignment = Assignment.find(params[:id])
+  end
+
+  def new
+    @assignment = Assignment.new
+  end
+
+  def create
+    @assignment = Assignment.new(assignment_params)
+
+    if @assignment.save
+      redirect_to assignment_path(@assignment)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @assignment = Assignment.find(params[:id])
+  end
+
+  def update
+    @assignment = Assignment.find(params[:id])
+
+    if @assignment.update_attributes(assignment_params)
+      redirect_to assignment_path(@assignment)
+    else
+      render 'edit'
+    end
+  end
+
+  def delete
+    @assignment = Assignment.find(params[:id])
+  end
+
+  def destroy
+    @assignment = Assignment.find(params[:id])
+    @assignment.destroy
+    redirect_to assignments_path
+  end
+
+  private
+
+  def assignment_params
+    params.require(:assignment).permit(:title, :content)
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.destroy_all
+Assignment.destroy_all
+
 User.create(first_name: "Kill", last_name: "Bill", username: "killb", email: "killb@dead.com", password: "dead", password_confirmation: "dead", admin: false)
 #User.create(first_name: "Nick", last_name: "Cage", username: "cagen", email: "nickcage@nickcageisawesome.com", password: "awesome", password_confirmation: "awesome", admin: false)
 User.create(first_name: "admin", last_name: "admin", username: "admin", email: "admin@admin.com", password: "admin", password_confirmation: "admin", admin: true)
-100.times do |x|
+50.times do |x|
   User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, username: Faker::Internet.user_name, email: Faker::Internet.safe_email, password: Faker::Internet.password(8), admin: false )
+end
+
+50.times do |x|
+  Assignment.create(title: Faker::Book.title, content: Faker::Lorem.paragraph(3, true, 5))
 end
