92d53b8030af7815181527c1ed4000ff805325b7
Michael Prather <mprather1@gmail.com>
2016-04-03 17:30:40 -0500

    seed file update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160403055050) do
+ActiveRecord::Schema.define(version: 20160403220801) do
 
   create_table "assignments", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20160403055050) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+  end
+
+  create_table "classrooms", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,10 +11,10 @@ Assignment.destroy_all
 User.create(first_name: "Kill", last_name: "Bill", username: "killb", email: "killb@dead.com", password: "dead", password_confirmation: "dead", admin: false)
 #User.create(first_name: "Nick", last_name: "Cage", username: "cagen", email: "nickcage@nickcageisawesome.com", password: "awesome", password_confirmation: "awesome", admin: false)
 User.create(first_name: "admin", last_name: "admin", username: "admin", email: "admin@admin.com", password: "admin", password_confirmation: "admin", admin: true)
-50.times do |x|
+10.times do |x|
   User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, username: Faker::Internet.user_name, email: Faker::Internet.safe_email, password: Faker::Internet.password(8), admin: false )
 end
 
 50.times do |x|
-  Assignment.create(title: Faker::Book.title, content: Faker::Lorem.paragraph(3, true, 5), user_id: Faker::Number.between(1, 50))
+  Assignment.create(title: Faker::Book.title, content: Faker::Lorem.paragraph(3, true, 5), user_id: Faker::Number.between(1, 10))
 end
